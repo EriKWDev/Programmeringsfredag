8139e6fcbc58fc48df4b76f0fb15155f7d19246f
EriKWDev <ErikWDev@gmail.com>
2018-06-26 10:59:08 +0200

    :3

--- Erik/Python/Euler003.py
+++ Erik/Python/Euler003.py
@@ -7,9 +7,17 @@ n = 600851475143
 i = 2
 
 def IsPrime (number):
-    for i in range(2, number):
-        if (number % i == 0):
+    if (number <= 1):
+        return False
+    elif (number <= 3):
+        return True
+    elif (number % 2 == 0 or number % 3 == 0):
+        return False
+    i = 5
+    while (i * i <= number):
+        if (number % i == 0 or number % (i + 2) == 0):
             return False
+        i += 6
     return True
 
 def NextPrime (p):
