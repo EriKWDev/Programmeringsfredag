b3b4f5240dc74c8dd97f3c972724939930681fd4
EriKWDev <ErikWDev@gmail.com>
2018-10-12 20:07:10 +0200

    Python Update

--- Erik/Python/Euler019.py
+++ Erik/Python/Euler019.py
@@ -5,7 +5,7 @@
 import datetime
 from datetime import timedelta
 
-startDate = datetime.datetime (1901, 01, 01)
+startDate = datetime.datetime (1901, 1, 1)
 endDate = datetime.datetime (2000, 12, 31)
 
 dayCount = (endDate - startDate).days + 1
@@ -15,4 +15,4 @@ for tmpDate in (startDate + timedelta(n) for n in range (dayCount)):
     if tmpDate.weekday () == 6 and tmpDate.day == 1:
         n += 1
 
-print n
+print (n)
