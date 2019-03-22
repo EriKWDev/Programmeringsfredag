a1dd386cf75ade0a58a8e2c41dadb363a644df8c
ErikWDev (new Mac) <ErikWDev@gmail.com>
2019-03-22 18:17:42 +0100

    :3

--- Erik/Python/IPyUtility/IPyUtility.py
+++ Erik/Python/IPyUtility/IPyUtility.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp, QTableWidget, QTableWidgetItem, QVBoxLayout
-
+from PyQt5.QtWidgets import QMenu, QApplication, QMainWindow, QAction, qApp, QVBoxLayout, QWidget, QTreeWidget, QTreeWidgetItem
+from PyQt5.QtCore import Qt
 
 class Window(QMainWindow):
 
@@ -19,54 +19,78 @@ class Window(QMainWindow):
 		help_menu = menu_bar.addMenu("Help")
 
 		# Create Menu Actions
-		quit_action = QAction("Exit", self)
-		quit_action.setShortcut("Ctrl+q")
+		self.quit_action = QAction("Exit", self)
+		self.quit_action.setShortcut("Ctrl+q")
 
-		help_action = QAction("About AXIS IP Utility", self)
-		help_action.setShortcut("Ctrl+h")
+		self.help_action = QAction("About AXIS IP Utility", self)
+		self.help_action.setShortcut("Ctrl+h")
 
-		assign_network_parameters_action = QAction("Assign Network Parameters", self)
-		assign_network_parameters_action.setShortcut("Ctrl+c")
+		self.assign_network_parameters_action = QAction("Assign Network Parameters", self)
+		self.assign_network_parameters_action.setShortcut("Ctrl+c")
 
-		assign_ip_address_action = QAction("Assign IP Address", self)
-		assign_ip_address_action.setShortcut("Ctrl+a")
+		self.assign_ip_address_action = QAction("Assign IP Address", self)
+		self.assign_ip_address_action.setShortcut("Ctrl+a")
 
-		assign_serial_no_ip_address_action = QAction("Assign IP Address Using Serial Number", self)
-		assign_serial_no_ip_address_action.setShortcut("Ctrl+n")
+		self.assign_serial_no_ip_address_action = QAction("Assign IP Address Using Serial Number", self)
+		self.assign_serial_no_ip_address_action.setShortcut("Ctrl+n")
 
-		test_ip_address_action = QAction("Test IP Address", self)
-		test_ip_address_action.setShortcut("Ctrl+t")
+		self.test_ip_address_action = QAction("Test IP Address", self)
+		self.test_ip_address_action.setShortcut("Ctrl+t")
+
+		self.open_in_web_browser = QAction("Open in Web Browser", self)
+		self.open_in_web_browser.setShortcut("Ctrl+o")
 
 		# Add Menu Actions
-		file_menu.addAction(quit_action)
+		file_menu.addAction(self.quit_action)
 
-		tools_menu.addAction(assign_network_parameters_action)
-		tools_menu.addAction(assign_ip_address_action)
-		tools_menu.addAction(assign_serial_no_ip_address_action)
-		tools_menu.addAction(test_ip_address_action)
+		tools_menu.addAction(self.assign_network_parameters_action)
+		tools_menu.addAction(self.assign_ip_address_action)
+		tools_menu.addAction(self.assign_serial_no_ip_address_action)
+		tools_menu.addAction(self.test_ip_address_action)
 
-		help_menu.addAction(help_action)
+		help_menu.addAction(self.help_action)
 
 		# List
-		device_table = QTableWidget()
-		device_table.setColumnCount(4)
-		devices = ["Camera1", "Camera2", "Camera3"]
-		for i in devices:
-			device_table.insertRow(1)
-			device_table.setItem()
+		devices = [
+		["Camera1", "192.168.0.10", "ACC8TEMP"],
+		["Camera2", "192.168.0.10", "ACC8TEMP"],
+		["Camera3", "192.168.0.10", "ACC8TEMP"]
+		]
+		main_widget = QWidget(self)
+		self.devices_tree = QTreeWidget()
+		self.devices_tree.headerItem().setText(0, "Name")
+		self.devices_tree.headerItem().setText(1, "IP Address")
+		self.devices_tree.headerItem().setText(2, "ID")
+
+		self.devices_tree.customContextMenuRequested.connect(self.open_menu)
+		self.devices_tree.setContextMenuPolicy(Qt.CustomContextMenu)
+
+		for i in range(0, len(devices)):
+			devices_tree_item = QTreeWidgetItem(self.devices_tree)
+			[devices_tree_item.setText(j, devices[i][j]) for j in range(0, 3)]
 
 		layout = QVBoxLayout()
-		layout.addWidget(device_table)
-		self.setLayout(layout)
+		layout.addWidget(self.devices_tree)
+		main_widget.setLayout(layout)
+		self.setCentralWidget(main_widget)
 
 		# Events
-		quit_action.triggered.connect(self.quit_trigger)
+		self.quit_action.triggered.connect(self.quit_trigger)
 
 		self.setWindowTitle("AXIS IP Utility in Python - Erik Wilhelm Gren 2019")
 		status_bar.showMessage("{} devices".format(len(devices)))
 		self.resize(640, 300)
 
 		self.show()
+
+	def open_menu(self, position):
+		menu = QMenu()
+
+		indexes = self.devices_tree.selectedIndexes()
+		print(indexes)
+
+		menu.addAction(self.open_in_web_browser)
+		menu.exec_(self.devices_tree.viewport().mapToGlobal(position))
 
 	def quit_trigger(self):
 		qApp.quit()
