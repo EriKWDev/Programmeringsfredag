eda08eb80257f6b2a8d6a612c9f82ad2c4a23678
ErikWDev (new Mac) <ErikWDev@gmail.com>
2019-04-26 08:48:53 +0200

    Recept

--- Erik/JavaScript/Recipes/recipes.js
+++ Erik/JavaScript/Recipes/recipes.js
@@ -3,6 +3,7 @@ const fs = require("fs-extra");
 const hbs = require("handlebars");
 const path = require("path");
 const merge = require('easy-pdf-merge');
+const FORCECREATE = true;
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
@@ -17,31 +18,45 @@ const compile = async function(templateName, data) {
 const createRecipePDF = async function (name) {
 	try {
 		console.log(`Creating PDF for "${name}.json"`);
+		console.time(name);
 
-		const browser = await puppeteer.launch();
-		const page = await browser.newPage();
-		const options = {
-			path: `pdf/${name}.pdf`,
-			format: "A4",
-			printBackground: true
-		};
-
-		const templateName = "template-2";
 		const data = require(path.join(process.cwd(), "recipes-data", `${name}.json`));
-		const prev_data = require(path.join(process.cwd(), "tmp", `${name}.json`));
 
-		if(JSON.stringify(data) == JSON.stringify(prev_data)) {
-			console.log("Recipe not changed. Aborting creation.");
-			return;
+		var prev_data;
+
+		try {
+			prev_data = require.resolve(path.join(process.cwd(), "tmp", `${name}.json`));
+		} catch (e) {
+			console.log("New Recipe");
+			prev_data = null;
 		}
 
-		console.log();
+		if(prev_data != null) {
+			prev_data = require(path.join(process.cwd(), "tmp", `${name}.json`));
+		}
+
+		if(!FORCECREATE && prev_data != null && JSON.stringify(data) == JSON.stringify(prev_data)) {
+			console.log("Recipe not changed. Aborting creation.");
+			console.timeEnd(name);
+			return;
+		}
 
 		await fs.writeFile(path.join(process.cwd(), "tmp", `${name}.json`), JSON.stringify(data), (e) => {
 			if(e) {
 				return console.log(e);
 			}
 		});
+
+		const templateName = "template-2";
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		const options = {
+			path: path.join(process.cwd(), "pdf", `${name}.pdf`),
+			format: "A4",
+			printBackground: true
+		};
+
+		data.main.firstLetter = data.main.title[0];
 
 		const recipeHTML = await compile(templateName, data);
 
@@ -51,14 +66,18 @@ const createRecipePDF = async function (name) {
 			}
 		});
 
-		const htmlPath = "file:" + path.join(process.cwd(), "html", `${name}.html`);
+		const htmlPath = path.join(process.cwd(), "html", `${name}.html`);
 		console.log("Path: " + htmlPath);
+		const htmlFile = await fs.read
 		await page.emulateMedia("print");
-		await page.goto(htmlPath, {waitUntil: "networkidle2"});
+		//await page.goto(`data:text/html,${recipeHTML}`, {waitUntil: "networkidle2"});
+		//await page.setContent(recipeHTML);
+		await page.goto("file:" + htmlPath, {waitUntil: "networkidle2"});
 		await page.pdf(options);
 
 		await browser.close();
 		console.log(`"${name}.pdf" created succesfully from "${name}.json"`);
+		console.timeEnd(name);
 		console.log("");
 		await sleep(800);
 		return;
@@ -89,6 +108,7 @@ const createAllRecipes = async function () {
 			return console.log("Error: ", error);
 		}
 
+		console.log("All Recipe-PDFs have been merged to _Recipes.pdf");
 		console.log("");
 		console.log("__________ Finnished Recipe Creation __________");
 
@@ -98,6 +118,7 @@ const createAllRecipes = async function () {
 		console.log("_________________ Quick Facts _________________");
 		console.log("");
 		console.log("Total number of Recipes: " + filePaths.length);
+		console.log("Forced? " + FORCECREATE);
 		console.log("_______________________________________________");
 		for(let i = 0; i < 3; i++) {
 			console.log("");
