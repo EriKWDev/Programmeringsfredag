3a9198853340fef14f061ad978ead46fd2459011
ErikWDev (new Mac) <ErikWDev@gmail.com>
2019-07-08 16:12:49 +0200

    React Native

---
+++ Erik/React-Native/FirstProject/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  SectionList,
+} from "react-native";
+
+const App = () => {
+  return (
+	<View>
+		<Text>Hello, World</Text>
+	</View>
+  );
+};
+
+export default App;
