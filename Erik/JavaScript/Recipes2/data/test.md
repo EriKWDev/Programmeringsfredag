cbdc663a9bd7c6a386f8b3ababd417b03318db92
ErikWDev (new Mac) <ErikWDev@gmail.com>
2019-11-03 11:18:30 +0100

    recipes2.0 css!!!

--- Erik/JavaScript/Recipes2/data/test.md
+++ Erik/JavaScript/Recipes2/data/test.md
@@ -1,19 +1,41 @@
-# Test
-yup
+---
+title: Spaghetti Bologna
+subtitle: Spaghetti med Köttfärsås
+stars: 8
+time: 1h
+---
 
-> Hello, World!
-> I am a quote!
+# Ingredienser
 
-lolololol
+## Sås
+1. Kött
+2. Tomat
+3. Basilika
+4. Vitlök
+5. Peppar
+6. Salt
 
-Lorem ipsum dolor sit amet
+## Pasta
+1. Mjöl
+2. Ägg
+3. Vatten
 
-# Title
+# Tillagning
 
-## Smaller title
-lololol
+## Sås
+1. Hacka tomater och lägg i gryta.
+2. Gör sås.
+3. Stek kött.
+4. Blanda i kött.
+5. Stek tills vätska försvinner..
 
-## lololol
-wdonwodhnwodno
+## Pasta
+1. Skrik på italienska.
+2. Fyll ett bord med mjöl.
+3. Gör äggvulkan.
+4. Knåda som bara den asså jävlar vad du kommer att behöva knåda.
+5. Platta ut degen och skär till pasta.
+6. Koka
+
 
 
--- Erik/JavaScript/Recipes2/main.js
+++ Erik/JavaScript/Recipes2/main.js
@@ -2,32 +2,113 @@ const fse = require("fs-extra")
 const md = require("markdown-it")()
 const path = require("path")
 const pdf = require("html-pdf")
+const yaml = require("yaml")
 
-const pdfOptions = {
-    "format": "A4",
-    "orientation": "portrait",
-    "border": {
-        "top": "1cm",
-        "right": "1cm",
-        "bottom": "2cm",
-        "left": "1cm"
+const options = {
+    "templateName":"template-001",
+    "pdfOptions":{
+        "format":"A3",
+        "orientation":"portrait",
+        "border":{
+            "top":"1cm",
+            "right":"1cm",
+            "bottom":"2cm",
+            "left":"1cm"
+        },
+        "base":`file://${path.join(__dirname, "data", "template-001.css")}`
     }
 }
 
-fse.readdir(path.join(__dirname, "data")).then((results) => {
-    results.forEach(fileName => {
-        fse.readFile(path.join(__dirname, "data", fileName), "utf-8").then((file) => {
-            if(fileName.split(".")[1] == "md") {
-                let html = `<link rel="stylesheet" type="text/css" href="data/template-1.css" />` + md.render(file)
-                console.log(html)
-                console.log(`Generating pdf for ${fileName.split(".")[0]}`)
-                pdf.create(html, pdfOptions).toFile(path.join(__dirname, "recipes", `${fileName.split(".")[0]}.pdf`), function(err, res) {
-                    if (err) {
-                      console.log(err)
-                    }
-                })
-            }
-        })
-    })
-})
+console.log(path.join(__dirname, "data", "template-001.css"))
 
+const generateRecipes = async () => {
+    let directory = await fse.readdir(path.join(__dirname, "data"))
+
+    for(let fileName of directory) {
+
+        if(fileName.split(".")[1] != "md") {
+            continue
+        }
+
+        let filePath = path.join(__dirname, "data", fileName)
+
+        let html = await getHTMLFromFile(filePath)
+        await generatePdfFromHTML(html, fileName)
+    }
+}
+
+const getHTMLFromFile = async (filePath) => {
+    let file = await fse.readFile(filePath, "utf-8")
+    let lines = file.split(`\n`)
+
+    let isReadingFrontMatter = false
+    let yamlFrontMatter = ""
+    let mdRecipe = ""
+
+    for(let line of lines) {
+        if(line == "---") {
+            isReadingFrontMatter = !isReadingFrontMatter;
+        } else if(isReadingFrontMatter) {
+            yamlFrontMatter += line + "\n"
+        } else {
+            mdRecipe += line + "\n"
+        }
+    }
+    
+    let frontMatter = await yaml.parse(yamlFrontMatter)
+    let renderedHTML = await md.render(mdRecipe)
+    console.log(frontMatter)
+
+    let html =
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <link rel="stylesheet" type="text/css" href="${options.templateName + ".css"}"
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=0.2">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+        </head>
+        <body>
+            <div class="container">
+
+                <div class="recipe-top">
+                    <h1>${frontMatter.title}</h1>
+                    <p>${frontMatter.subtitle}</p>
+                    <p>${frontMatter.time}</p>
+                    <div class="stars-container">${generateStarsHTML(frontMatter.stars)}</div>
+                </div>
+
+                <div class="recipe-container">
+                    ${renderedHTML}
+                </div>
+
+            </div>
+        </body>
+    </html>
+    `
+
+    await fse.writeFile(filePath.split(".")[0] + ".html", html, "utf-8")
+    return html
+}
+
+const generatePdfFromHTML = async (html, fileName) => {
+    console.log(`Generating pdf for ${fileName}...`)
+    pdf.create(html, options.pdfOptions).toFile(path.join(__dirname, "recipes", `${fileName.split(".")[0]}.pdf`), (err, res) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+}
+
+const generateStarsHTML = (stars) => {
+    let html = `<span class="stars">`
+    for(let i = 0; i < 10; i++) {
+        html += `<i class="${i < stars ? "star" : "no-star"}"> </i>`
+    }
+    html += `</span>`
+    return html
+}
+
+generateRecipes()
